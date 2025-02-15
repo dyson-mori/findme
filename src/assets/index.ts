257429762e7128e4f13f723df0104f4b350a8895
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2025-02-15 08:34:05 -0300

    :boom: massive export

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import { ReactComponent as Database } from './database.svg';
+import { ReactComponent as User } from './user.svg';
+import { ReactComponent as Google } from './google.svg';
+
+export {
+  Database,
+  User,
+  Google
+};
