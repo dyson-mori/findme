f90a6c6a3580b5aedf7727d3102cee19fe3273dc
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2025-02-15 08:37:11 -0300

    :sparkles: button

---
+++ src/common/button/index.tsx
@@ -0,0 +1,26 @@
+import React, { ButtonHTMLAttributes, forwardRef } from 'react';
+
+import { Container, Loading } from './styles';
+
+export type Variant = 'primary' | 'select' | 'selected' | 'error' | 'loading' | 'icon';
+
+type ButtonProps = {
+  variant?: Variant;
+  loading?: boolean;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+const loading_css = (
+  <Loading>
+    <div className="lds-dual-ring"></div>
+  </Loading>
+);
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ variant = 'primary', children, ...rest }, ref) => (
+    <Container ref={ref} variant={variant} disabled={variant === 'loading'} {...rest}>
+      {variant === 'loading' ? loading_css : children}
+    </Container>
+  )
+);
+
+Button.displayName = 'Button';
